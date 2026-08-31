ae0d52bc4d3ec0ed1e8d08d31e8c972b3ad1021f
Admin <admin@ulmap.se>
2026-08-31 15:07:48 +0200

    update nav

--- JobySaasFrontend/Data/Migrations/20260831130037_2222.Designer.cs
+++ JobySaasFrontend/Data/Migrations/20260831130037_2222.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using JobySaasFrontend.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace JobySaasFrontend.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260831130037_2222")]
+    partial class _2222
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "10.0.7");
---
+++ JobySaasFrontend/Data/Migrations/20260831130037_2222.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobySaasFrontend.Migrations
+{
+    /// <inheritdoc />
+    public partial class _2222 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProfileImageUrl",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProfileImageUrl",
+                table: "AspNetUsers");
+        }
+    }
+}
