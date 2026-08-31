535e8d40d52b5ec4bb5526361b48d02acca418fa
Admin <admin@ulmap.se>
2026-08-31 13:21:29 +0200

    add more info in social list and change look on liist

--- JobySaasFrontend/Models/DTO/SocialAccountDto.cs
+++ JobySaasFrontend/Models/DTO/SocialAccountDto.cs
@@ -3,8 +3,31 @@ namespace JobySaasFrontend.Models.DTO;
 public class SocialAccountDto
 {
     public Guid Id { get; set; }
-    public string Platform { get; set; } // "YouTube", "Instagram", etc.
+    public SocialAccountPlatfrom Platform { get; set; } // "YouTube", "Instagram", etc.
     public string AccountName { get; set; }
+    public string CostumUrl {get; set;}
     public string ProfileImageUrl { get; set; }
+    public ulong? Followers {get; set;}
+    public SocialAccountStatus Status {get; set;}
+
+    public DateTime LastSync {get; set;}
     public bool IsActive { get; set; }
+}
+
+public enum SocialAccountStatus
+{
+    Connected,
+    Expired,
+    Error,
+    Disconnected
+}
+
+public enum SocialAccountPlatfrom
+{
+    YouTube,
+    TikTok,
+    X,
+    LinkedIn,
+    Instagram,
+    Facebook
 }
